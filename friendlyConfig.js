1652fef9244c63c6d7f92bb401813ed22e0120ad
Damian Kaczmarek <rush@rushbase.net>
2013-12-28 18:16:18 +0100

    Added simple setup case.

--- friendlyConfig.js
+++ friendlyConfig.js
@@ -1,9 +1,23 @@
+var assert = require('assert');
+
+var XRegExp = require('xregexp').XRegExp;
+
+XRegExp.install({
+  // Overrides native regex methods with fixed/extended versions that support named
+  // backreferences and fix numerous cross-browser bugs
+  natives: true,
+
+  // Enables extensibility of XRegExp syntax and flags
+  extensibility: true
+});
+
 module.exports = function(config) {
-  var domains = {};
+  var domains = config.domains || {};
 
   if (!config.ports)
     config.ports = {};
 
+  var ports = config.ports;
 
   if (typeof config.http == 'boolean') {
     config.http = [80];
@@ -38,8 +52,20 @@ module.exports = function(config) {
 
   Object.keys(domains).forEach(function(domain) {
     var domainEntry = domains[domain];
+    
+    if(typeof domainEntry == 'object') {
+    }
+    else { // assume it is int/string
+      assert(typeof domainEntry == 'string' || typeof domainEntry == 'number', 'port num');
+      var destination = domainEntry;
+      var m = domain.match(new XRegExp("^(?<host>[^:]*)(?::(?<port>\\d+))?$"));
+
+      assert(m.port, "port should be defined");
+
+      ports[m.port][m.host] = domainEntry;
 
 
+    }
 
   });
 
