d916ff1d974ce8839a3020a180cf20fa3ee5ef79
Damian Kaczmarek <rush@rushbase.net>
2013-12-28 16:59:53 +0100

    Handle http/https ports.

--- friendlyConfig.js
+++ friendlyConfig.js
@@ -1,12 +1,44 @@
 module.exports = function(config) {
   var domains = {};
 
-  if(!config.ports)
+  if (!config.ports)
     config.ports = {};
 
 
+  if (typeof config.http == 'boolean') {
+    config.http = [80];
+  }
+  if (typeof config.https == 'boolean') {
+    config.https = [443];
+  }
+
+  if (config.http) {
+    config.http.forEach(function(portEntry) {
+      config.ports[portEntry.port || portEntry] = {};
+      if(portEntry.port)
+        delete portEntry.port;
+    });
+  }
+
+
+  var sslConfig = {};
+
+  if (config.https) {
+    config.https.forEach(function(portEntry) {
+
+
+      config.ports[portEntry.port || portEntry] = {
+
+        ssl: typeof portEntry === 'object' ? portEntry : sslConfig
+      };
+      if(portEntry.port)
+        delete portEntry.port;
+    });
+  }
+
   Object.keys(domains).forEach(function(domain) {
     var domainEntry = domains[domain];
+
 
 
   });
--- tests/friendlyConfig.js
+++ tests/friendlyConfig.js
@@ -12,19 +12,38 @@ describe('domains config processor', function() {
       https: [443, 80443]
     };
     var config = processConfig(config);
-    config.should.have.key("ports");
-    config.ports.should.have.key("80");
-    config.ports.should.have.key("8080");
-    config.ports.should.have.key("443");
-    config.ports.should.have.key("80443");
+    config.should.have.property("ports");
+    config.ports.should.have.property("80");
+    config.ports.should.have.property("8080");
+    config.ports.should.have.property("443");
+    config.ports.should.have.property("80443");
+  });
+
+  it('should support ssl config per port', function() {
+    var config = {
+      https: [{port: 443, cipherList: ['CIPHER1', 'CIPHER2'], spdy: true}]
+    };
+    var config = processConfig(config);
+    config.should.have.property("ports");
+    config.ports.should.have.property("443");
+    config.ports[443].should.have.property('ssl');
+    config.ports[443].ssl.should.have.property('cipherList');
 
 
   });
 
+  it('should support simplified http entry', function() {
+    var config = {
+      http: true,
+      https: true
+    };
+    var config = processConfig(config);
+    config.should.have.property("ports");
+    config.ports.should.have.property("80");
+    config.ports.should.have.property("443");
+  });
+
   it('should support simple string keys', function() {
-
-
-
 
   });
 
