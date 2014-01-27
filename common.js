d42e09a51c39743aa742cea31e4c3adf73a54939
Damian Kaczmarek <rush@rushbase.net>
2014-01-27 15:21:29 +0100

    Split all worker logic to separate file. Hopefully this will allow future support for running http-master also as a module.

---
+++ common.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var modules = require("fs").readdirSync(path.join(__dirname, "modules")).map(function(file) {
+  return require('./modules/' + file);
+}).sort(function(a, b) { // sort modules according to priority
+  return (b.priority || 0) - (a.priority || 0);
+});
+
+function runModules(cb) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  var results = [];
+
+  var name;
+  if (typeof cb === 'string') {
+    name = cb;
+  } else {
+    name = args.shift();
+  }
+
+  modules.forEach(function(module) {
+    if (module[name]) {
+      var ret = module[name].apply(module[name], args);
+      if (ret && typeof cb === 'function')
+        cb(name, ret);
+    }
+  });
+}
+
+exports.runModules = runModules;
