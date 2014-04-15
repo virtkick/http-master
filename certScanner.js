1b863de0f7dd9af8f8c68d83b0ee818b109f14fb
Damian Kaczmarek <rush@rushbase.net>
2014-04-15 14:47:44 +0200

    More async improvements

--- certScanner.js
+++ certScanner.js
@@ -14,33 +14,32 @@ module.exports = function(sslDirectory) {
   this.scan = function(cb) {
     var config = {};
 
-    var files = fs.readdirSync(this.sslDirectory);
+    fs.readdir(this.sslDirectory, function(err, files) {
+      if(err) return cb(err);
+      
+      async.each(files, function(certFile, cb) {
+        var certPath = that.sslDirectory + certFile;
 
+        that.getDomainsFrom(certPath, function(err, altNames) {
+          if(err) return cb(err);
 
-    async.each(files, function(certFile, cb) {
-      var certPath = that.sslDirectory + certFile;
+          async.each(altNames, function(domain, cb) {
+            config[domain] = {};
+            config[domain].cert = certPath;
 
-      that.getDomainsFrom(certPath, function(err, altNames) {
-        if(err) return cb(err);
+            that.getCaFor(certPath, function(err, ca) {
+              if (ca) {
+                config[domain].ca = ca;
+              }
+              cb(err);
+            });
+          }, cb);
 
-        async.each(altNames, function(domain, cb) {
-          config[domain] = {};
-          config[domain].cert = certPath;
-
-          that.getCaFor(certPath, function(err, ca) {
-            if (ca) {
-              config[domain].ca = ca;
-            }
-            cb(err);
-          });
-        }, cb);
-
+        });
+      }, function(err) {
+        cb(err, config);
       });
-    }, function(err) {
-      cb(err, config);
     });
-
-    //cb(config);
   };
 
   this.getDomainsFrom = function(certPath, cb) {
@@ -56,34 +55,37 @@ module.exports = function(sslDirectory) {
   };
 
   this.getCaFor = function(certPath, cb) {
-    var rawCert = fs.readFileSync(certPath).toString();
-    var expectedIssuer = x509.parseCert(rawCert).issuer;
-
-    fs.readdir(this.sslDirectory, function(err, files) {
+    fs.readFile(certPath, 'utf8', function(err, rawCert) {
       if(err) return cb(err);
 
-      async.filter(files, function(certFile, cb) {
-        var certPath = that.sslDirectory + certFile;
-        that.getCaCertsFromFile(certPath, function(err, certs) {
-          if (that.isDomainCert(certs)) {
-            return cb(false);
-          }
-          return cb(certs.some(function(cert) {
-            return that.issuerMatches(cert, expectedIssuer);
-          }));
-        });
-      }, function(ca) {
-        ca = ca.map(function(fileName) {
-          return that.sslDirectory + fileName;
-        });
+      var expectedIssuer = x509.parseCert(rawCert).issuer;
 
-        if (ca.length === 0) {
-          cb(null, null);
-        } else if (ca.length === 1) {
-          cb(null, ca[0]);
-        } else {
-          cb(null, ca);
-        }
+      fs.readdir(that.sslDirectory, function(err, files) {
+        if(err) return cb(err);
+
+        async.filter(files, function(certFile, cb) {
+          var certPath = that.sslDirectory + certFile;
+          that.getCaCertsFromFile(certPath, function(err, certs) {
+            if (that.isDomainCert(certs)) {
+              return cb(false);
+            }
+            return cb(certs.some(function(cert) {
+              return that.issuerMatches(cert, expectedIssuer);
+            }));
+          });
+        }, function(ca) {
+          ca = ca.map(function(fileName) {
+            return that.sslDirectory + fileName;
+          });
+
+          if (ca.length === 0) {
+            cb(null, null);
+          } else if (ca.length === 1) {
+            cb(null, ca[0]);
+          } else {
+            cb(null, ca);
+          }
+        });
       });
     });
   };
@@ -121,7 +123,7 @@ module.exports = function(sslDirectory) {
         try {
           certs.push(x509.parseCert(parsedCert));
         } catch(err) {
-          
+
         }
       });
       cb(null, certs);
