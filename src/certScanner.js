86753e378b2032826239a64f05eeb4a3fa547789
Damian Kaczmarek <rush@rushbase.net>
2016-01-07 15:19:02 +0100

    Refactor cert scanner to a class, fix handling cert dates, update deps
    Fixes #118

--- src/certScanner.js
+++ src/certScanner.js
@@ -9,44 +9,173 @@ var util = require('util');
 
 var EventEmitter = require('eventemitter3');
 
-module.exports = function(sslDirectory, options) {
-  var that = this;
-  options = options || {};
+// TODO: rework into promises
+class CertScanner extends EventEmitter {
+  constructor(sslDirectory, options) {
+    super();
+    
+    this.options = options;
+    
+    options = options || {};
 
-  if(!sslDirectory) {
-    throw new Error('sslDirectory as first argument is mandatory');
+    if(!sslDirectory) {
+      throw new Error('sslDirectory as first argument is mandatory');
+    }
+
+    this.sslDirectory = sslDirectory;
+    if (!sslDirectory.match(/\/$/)) {
+      this.sslDirectory += '/';
+    }
+
+    this.readPart = async.memoize(function(file, maxRead, cb) {
+      fs.open(file, 'r', function(err, fd) {
+        if(err) return cb(err);
+        var buf = new Buffer(maxRead);
+        fs.read(fd, buf, 0, maxRead, null, function(err, bytesRead, buffer) {
+          fs.close(fd);
+          if(bytesRead < maxRead)
+            return cb(err, buffer.slice(0, bytesRead).toString('utf8'));
+          return cb(err, buffer.toString('utf8'));
+        });
+      });
+    });
+
+    var beginCertToken = '-----BEGIN CERTIFICATE-----';
+    var endCertToken = '-----END CERTIFICATE-----';
+    this.getCaCertsFromFile = function(certPath, cb) {
+      // TODO: This can possibly be replaced with some regexp.
+      this.readPart(certPath, 4*65536, function(err, certFileContent) {
+        if(err) return cb(err);
+
+        var possibleCerts = certFileContent.split(beginCertToken);
+        var certs = [];
+        var rawCerts = [];
+        possibleCerts.forEach(function(cert) {
+          var endTokenIndex = cert.indexOf(endCertToken);
+          if (endTokenIndex === -1) {
+            return null;
+          }
+          var rawCert = cert.substring(0, endTokenIndex);
+          var parsedCert = beginCertToken + rawCert + endCertToken + '\n';
+          try {
+            certs.push(x509.parseCert(parsedCert));
+            rawCerts.push(parsedCert);
+          } catch(err) {
+
+          }
+        });
+        cb(null, certs, rawCerts);
+      });
+    };
+    
+    this.getCaFor = async.memoize((certPath, cb) => {
+      this.readPart(certPath, 65636, (err, rawCert) => {
+        if(err) return cb(err);
+
+        var parsedCert;
+        try {
+          parsedCert = x509.parseCert(rawCert);
+        } catch(err) {
+          return cb(err);
+        }
+        var caResults = [];
+        var caRawResults = [];
+        let processDirectory = (dirName, cb) => {
+          fs.readdir(dirName, (err, files) => {
+
+            if(err) return cb(err);
+
+            async.filter(files, (certFile, cb) => {
+              var certPath = path.join(dirName, certFile);
+
+              fs.stat(certPath, (err, statData) => {
+                if(statData.isDirectory()) {
+                  return processDirectory(certPath, () => {
+                    cb(false); // is a directory
+                  });
+                }
+
+                this.getCaCertsFromFile(certPath, (err, certs, rawCerts) => {
+                  if(err) return cb(false);
+
+                  if (this.isDomainCert(certs)) {
+                    return cb(false);
+                  }
+                  var matchingCa = certs.filter((cert, i) => {
+                    var res = this.caMatches(cert, parsedCert);
+
+                    if(res) {
+                      caRawResults.push(rawCerts[i]);
+                    }
+                    return res;
+                  });
+                  return cb(matchingCa.length);
+                });
+              });
+            }, function(ca) {
+              caResults = ca.map(fileName => {
+                return path.join(dirName, fileName);
+              }).concat(caResults);
+
+              cb(null);
+            });
+          });
+        }
+        processDirectory(this.sslDirectory, err => {
+          if(err) return cb(err);
+
+          function noArrayIfOne(arr) {
+            return (arr.length === 1) ? arr[0] : arr;
+          }
+
+          if(caResults.length) {
+            cb(null, noArrayIfOne(caResults), noArrayIfOne(caRawResults));
+          }
+          else {
+            cb(null);
+          }
+        });
+      });
+    });
   }
-
-  this.sslDirectory = sslDirectory;
-  if (!sslDirectory.match(/\/$/)) {
-    this.sslDirectory += '/';
-  }
-
-  this.scan = function(cb) {
+  
+  scan(cb) {
     var outputConfig = {};
 
     var keys = {};
     var certs = {};
 
-    function processDirectory(dirName, cb) {
-      fs.readdir(dirName, function(err, files) {
+    let options = this.options;
+
+    let processDirectory = (dirName, cb) => {
+      fs.readdir(dirName, (err, files) => {
         if(err) return cb(err);
 
-        async.each(files, function(certFile, cb) {
+        async.each(files, (certFile, cb) => {
           var certPath = path.join(dirName, certFile);
 
-          fs.stat(certPath, function(err, statData) {
+          fs.stat(certPath, (err, statData) => {
             if(err) return cb(err);
 
             if(statData.isDirectory())
-              return processDirectory(certPath, cb);
+              return processDirectory(certPath, err => {
+                if(err && err.code === 'EACCES') {
+                  this.emit('notice', path.join(dirName, certPath) + ': directory is not accessible');
+                  return cb(null);
+                }
+                cb(err);
+              });
 
-            that.getCertOrPem(certPath, function(err, cert, pem, rawCert) {
+            this.getCertOrPem(certPath, (err, cert, pem, rawCert) => {
               if(pem) {
                 keys[pem.publicModulus] = options.read ? rawCert : certPath;
                 return cb();
               }
               if(err)  {
+                if(err.code === 'EACCES') {
+                  this.emit('notice', path.join(dirName, certPath) + ': file is not readable');
+                  return cb(null);
+                }
                 if(err.toString().match(/Certificate data larger than/))
                   return cb(null);
                 if(err.toString().match(/Unable to parse/))
@@ -61,19 +190,24 @@ module.exports = function(sslDirectory, options) {
 
 
               if(!options.acceptInvalidDates) {
-                var dateFormat = 'MMM DD HH:mm:ss YYYY';
-                if(moment.parseZone(cert.notBefore, dateFormat).diff(moment()) < 0) { // valid
-                  if(moment.parseZone(cert.notAfter, dateFormat).diff(moment()) > 0) { // valid
-                    if(moment.parseZone(cert.notAfter, dateFormat).diff(moment(), 'd') < 90)
-                      that.emit('notice', path.join(dirName, certPath) + ': valid only for ' + moment(new Date(cert.notAfter)).diff(moment(), 'd').toString() + ' days');
+                var notBeforeMoment = moment(new Date(cert.notBefore));
+                var notAfterMoment = moment(new Date(cert.notAfter));
+                var nowMoment = moment();
+                if(notBeforeMoment.diff(nowMoment) < 0) { // valid
+                  if(notAfterMoment.diff(nowMoment) > 0) { // valid
+                    if(notAfterMoment.diff(nowMoment, 'd') < 90) {
+                      var daysValid = notAfterMoment.diff(nowMoment, 'd').toString();
+                      var hoursValid = notAfterMoment.diff(nowMoment, 'h') - daysValid*24;
+                      this.emit('notice', path.join(dirName, certPath) + ': valid only for ' + daysValid + ' days ' + hoursValid + ' hours');
+                    }
                   }
                   else { //expired
-                    that.emit('notice', path.join(dirName, certPath) + ': expired ' + (-moment(new Date(cert.notAfter)).diff(moment(), 'd')).toString() + ' days ago');
+                    this.emit('notice', path.join(dirName, certPath) + ': expired ' + (-notAfterMoment.diff(nowMoment, 'd')).toString() + ' days ago');
                     return cb();
                   }
                 }
                 else { // not yet valid
-                  that.emit('notice', path.join(dirName, certPath) + ': not yet valid for ' + (moment(new Date(cert.notBefore)).diff(moment(), 'd')).toString() + ' days');
+                  this.emit('notice', path.join(dirName, certPath) + ': not yet valid for ' + (notBeforeMoment.diff(nowMoment, 'd')).toString() + ' days');
                   return cb();
                 }
               }
@@ -85,11 +219,11 @@ module.exports = function(sslDirectory, options) {
               var keyForCert = options.read ? rawCert : certPath;
               certs[keyForCert] = cert.publicModulus;
 
-              async.each(altNames, function(domain, cb) {
+              async.each(altNames, (domain, cb) => {
                 outputConfig[domain] = {};
                 outputConfig[domain].cert = options.read ? rawCert : certPath;
 
-                that.getCaFor(certPath, function(err, ca, caRaw) {
+                this.getCaFor(certPath, (err, ca, caRaw) => {
                   if (ca) {
                     outputConfig[domain].ca = options.read ? caRaw : ca;
                   }
@@ -135,23 +269,10 @@ module.exports = function(sslDirectory, options) {
 
       cb(err, outputConfig);
     });
-  };
-
-  var readPart = async.memoize(function(file, maxRead, cb) {
-    fs.open(file, 'r', function(err, fd) {
-      if(err) return cb(err);
-      var buf = new Buffer(maxRead);
-      fs.read(fd, buf, 0, maxRead, null, function(err, bytesRead, buffer) {
-        fs.close(fd);
-        if(bytesRead < maxRead)
-          return cb(err, buffer.slice(0, bytesRead).toString('utf8'));
-        return cb(err, buffer.toString('utf8'));
-      });
-    });
-  });
-
-  this.getCertOrPem = function(certPath, cb) {
-    readPart(certPath, 65536, function(err, rawCert) {
+  }
+  
+  getCertOrPem(certPath, cb) {
+    this.readPart(certPath, 65536, function(err, rawCert) {
       if(err) return cb(err);
       try {
         var cert = x509.parseCert(rawCert);
@@ -170,89 +291,16 @@ module.exports = function(sslDirectory, options) {
     });
   }
 
-  this.getCaFor = async.memoize(function(certPath, cb) {
-    readPart(certPath, 65636, function(err, rawCert) {
-      if(err) return cb(err);
-
-      var parsedCert;
-      try {
-        parsedCert = x509.parseCert(rawCert);
-      } catch(err) {
-        return cb(err);
-      }
-      var caResults = [];
-      var caRawResults = [];
-      function processDirectory(dirName, cb) {
-        fs.readdir(dirName, function(err, files) {
-
-          if(err) return cb(err);
-
-          async.filter(files, function(certFile, cb) {
-            var certPath = path.join(dirName, certFile);
-
-            fs.stat(certPath, function(err, statData) {
-
-              if(statData.isDirectory()) {
-                return processDirectory(certPath, function() {
-                  cb(false); // is a directory
-                });                
-              }
-
-              that.getCaCertsFromFile(certPath, function(err, certs, rawCerts) {
-                if(err) return cb(false);
-
-                if (that.isDomainCert(certs)) {
-                  return cb(false);
-                }
-                var matchingCa = certs.filter(function(cert, i) {
-                  var res = that.caMatches(cert, parsedCert);
-
-                  if(res) {
-                    caRawResults.push(rawCerts[i]);
-                  }
-                  return res;
-                });
-                return cb(matchingCa.length);
-              });
-            });
-          }, function(ca) {
-            caResults = ca.map(function(fileName) {
-              return path.join(dirName, fileName);
-            }).concat(caResults);
-
-            cb(null);
-          });
-        });
-      }
-      processDirectory(that.sslDirectory, function(err) {
-        if(err) return cb(err);
-
-        function noArrayIfOne(arr) {
-          return (arr.length === 1) ? arr[0] : arr;
-        }
-
-        if(caResults.length) {
-          cb(null, noArrayIfOne(caResults), noArrayIfOne(caRawResults));
-        }
-        else {
-          cb(null);
-        }
-      });
-    });
-  });
-
-  this.isDomainCert = function(certs) {
+  isDomainCert(certs) {
     return certs.some(function(cert) {
       return cert.altNames.length > 0;
     });
-  };
+  }
 
-  this.caMatches = function(caCert, issuedCertificatedbyCA) {
-    var subject = caCert.subject;
-
-    var expectedIssuer = issuedCertificatedbyCA.issuer;
-
-    var status =
+  caMatches(caCert, issuedCertificatedbyCA) {
+    let subject = caCert.subject;
+    let expectedIssuer = issuedCertificatedbyCA.issuer;
+    let status =
      expectedIssuer.countryName === subject.countryName
         && expectedIssuer.organizationName === subject.organizationName
         && expectedIssuer.organizationalUnitName === subject.organizationalUnitName
@@ -261,37 +309,8 @@ module.exports = function(sslDirectory, options) {
        && caCert.publicExponent === issuedCertificatedbyCA.publicExponent
         ;
 
-        return status;
-  };
+    return status;
+  }
+}
 
-  var beginCertToken = '-----BEGIN CERTIFICATE-----';
-  var endCertToken = '-----END CERTIFICATE-----';
-  this.getCaCertsFromFile = function(certPath, cb) {
-    // TODO: This can possibly be replaced with some regexp.
-    readPart(certPath, 4*65536, function(err, certFileContent) {
-      if(err) return cb(err);
-
-      var possibleCerts = certFileContent.split(beginCertToken);
-      var certs = [];
-      var rawCerts = [];
-      possibleCerts.forEach(function(cert) {
-        var endTokenIndex = cert.indexOf(endCertToken);
-        if (endTokenIndex === -1) {
-          return null;
-        }
-        var rawCert = cert.substring(0, endTokenIndex);
-        var parsedCert = beginCertToken + rawCert + endCertToken + '\n';
-        try {
-          certs.push(x509.parseCert(parsedCert));
-          rawCerts.push(parsedCert);
-        } catch(err) {
-
-        }
-      });
-      cb(null, certs, rawCerts);
-    });
-  };
-};
-
-
-util.inherits(module.exports, EventEmitter);
+module.exports = CertScanner;
